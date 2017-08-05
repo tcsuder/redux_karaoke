0e61b9de90d595cff4a27528c6b806be3da2cb89
tcsuder@gmail.com <tcsuder@gmail.com>
2017-08-05 13:06:33 -0700

    add anonymous state object with song info

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+
+let chorus = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye"
+
+let chorusArray = chorus.split(', ');
+let position = 0;
+
+let initialState = {
+  chorusString: chorus,
+  chorusArray: chorusArray,
+  arrayPosition: position,
+  currentPhrase: chorusArray[position]
+}
+
+console.log(initialState);
+
 const userClick = () => {
   console.log('click');
 }
